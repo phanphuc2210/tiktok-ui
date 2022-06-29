c6eeb1f02ed355c4fa262467639910189facd1b2
phanphuc2210 <phucpth2001@gmail.com>
2022-06-29 19:00:49 +0700

    Cấu hình router các kiểu...

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+// Layout
+import { HeaderOnly } from '~/components/Layout';
+
+// Pages
+import Home from '~/pages/Home';
+import Following from '~/pages/Following';
+import Upload from '~/pages/Upload';
+import Search from '~/pages/Search';
+
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
