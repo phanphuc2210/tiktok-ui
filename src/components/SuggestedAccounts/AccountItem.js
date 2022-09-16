258a8f5aa1d144a5ae6a8b569afd30ee95361c51
phanphuc2210 <phucpth2001@gmail.com>
2022-09-16 13:57:53 +0700

    Sidebar: Account Preview

--- src/components/SuggestedAccounts/AccountItem.js
+++ src/components/SuggestedAccounts/AccountItem.js
@@ -1,43 +1,43 @@
 import classNames from 'classnames/bind';
-// import Tippy from '@tippyjs/react/headless';
+import Tippy from '@tippyjs/react/headless';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 
-// import { Wrapper as PopperWrapper } from '~/components/Popper';
-// import AccountPreview from './AccountPreview/AccountPreview';
+import { Wrapper as PopperWrapper } from '~/components/Popper';
+import AccountPreview from './AccountPreview';
 import styles from './SuggestedAccounts.module.scss';
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
-    // const renderPreview = (props) => {
-    //     return (
-    //         <div tabIndex="-1" {...props}>
-    //             <PopperWrapper>
-    //                 <AccountPreview />
-    //             </PopperWrapper>
-    //         </div>
-    //     );
-    // };
+    const renderPreview = (props) => {
+        return (
+            <div tabIndex="-1" {...props}>
+                <PopperWrapper>
+                    <AccountPreview />
+                </PopperWrapper>
+            </div>
+        );
+    };
 
     return (
         <div>
-            {/* <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}> */}
-            <div className={cx('account-item')}>
-                <img
-                    className={cx('avatar')}
-                    src="https://reviewdao.vn/wp-content/uploads/2020/12/tim-hieu-ve-ca-nhac-si-son-tung-mtp-4.jpg"
-                    alt=""
-                />
-                <div className={cx('item-info')}>
-                    <p className={cx('nickname')}>
-                        <strong>quocnguyenphu</strong>
-                        <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
-                    </p>
-                    <p className={cx('name')}>Quốc Nguyễn Phú</p>
+            <Tippy interactive delay={[800, 0]} offset={[-20, 0]} placement="bottom" render={renderPreview}>
+                <div className={cx('account-item')}>
+                    <img
+                        className={cx('avatar')}
+                        src="https://reviewdao.vn/wp-content/uploads/2020/12/tim-hieu-ve-ca-nhac-si-son-tung-mtp-4.jpg"
+                        alt=""
+                    />
+                    <div className={cx('item-info')}>
+                        <p className={cx('nickname')}>
+                            <strong>quocnguyenphu</strong>
+                            <FontAwesomeIcon className={cx('check')} icon={faCheckCircle} />
+                        </p>
+                        <p className={cx('name')}>Quốc Nguyễn Phú</p>
+                    </div>
                 </div>
-            </div>
-            {/* </Tippy> */}
+            </Tippy>
         </div>
     );
 }
